6248c59401981ae55499dcfa648705de959fc4a9
Astha <astha132btcse19@igdtuw.ac.in>
2021-09-04 22:12:28 +0530

    updated

--- Dicee/lib/main.dart
+++ Dicee/lib/main.dart
@@ -24,6 +24,12 @@ class DicePage extends StatefulWidget {
 class _DicePageState extends State<DicePage> {
   int leftDiceNumber = 1;
   int rightDiceNumber = 1;
+  void changeDiceFace(){
+	setState(() {
+	rightDiceNumber = Random().nextInt(6) + 1;
+	leftDiceNumber = Random().nextInt(6) + 1;
+    });
+}
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -32,22 +38,17 @@ class _DicePageState extends State<DicePage> {
           Expanded(
             child: TextButton(
               onPressed: () {
-                setState(() {
-                  leftDiceNumber = Random().nextInt(6) + 1;
-                });
-              },
+                changeDiceFace();
+		},
               child: Image.asset('images/dice$leftDiceNumber.png'),
             ),
           ),
+
           Expanded(
             child: TextButton(
               onPressed: () {
-                setState(
-                  () {
-                    rightDiceNumber = Random().nextInt(6) + 1;
-                  },
-                );
-              },
+                changeDiceFace();
+		},
               child: Image.asset('images/dice$rightDiceNumber.png'),
             ),
           ),
